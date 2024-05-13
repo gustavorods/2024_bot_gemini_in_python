7f64821aa494ddd62d0d10425bb55899a075ef49
gustavorods <gustavo.rlsilva07@gmail.com>
2024-05-13 09:41:30 -0300

    refactor bot.py: Code with more annotations

--- bot.py
+++ bot.py
@@ -1,55 +1,43 @@
-"""
-At the command line, only need to run once to install the package via pip:
-
-$ pip install google-generativeai
-"""
-
+# Importing necessary libraries
 import google.generativeai as genai
+import numpy as np
+import pandas as pd
 from dotenv import load_dotenv
 import os
+
+# Loading environment variables
 load_dotenv()
 
-genai.configure(api_key = os.getenv('API_KEY')) # It needs to be this specific way for the request to work
-  
-# Set up the model
+# Configuring API key
+genai.configure(api_key=os.getenv('API_KEY'))
+
+# Setting up model generation configuration
 generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 0,
-  "max_output_tokens": 8192,
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 0,
+    "max_output_tokens": 8192,
 }
 
+# Safety settings for content generation
 safety_settings = [
-  {
-    "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
 ]
 
-
+# Initializing the generative model
 model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
-convo = model.start_chat(history=[
-])
+# Starting a conversation with the generative model
+convo = model.start_chat(history=[])
 
- 
+# Chat loop
 prompt = ""
 while prompt != "fim":
-  prompt = input("Digite sua pergunta: ")
-  convo.send_message(prompt)
-  print(convo.last.text)
-
+    prompt = input("Digite sua pergunta: ")
+    convo.send_message(prompt)
+    print(convo.last.text)
